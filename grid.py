8922fe71ebd82f0311901733ccb9195d87829ef8
Parth <parthshreya@hotmail.com>
2020-08-04 21:46:21 -0400

    Update grid.py

--- grid.py
+++ grid.py
@@ -9,34 +9,50 @@ white = (255,255,255)
 red = (255,0,0)
 green = (0,255,0)
 blue = (0,0,255)
-class box(object):
-    def __init__(self,x,y,l,w,color):
-        self.x = x
-        self.y = y
-        self.l = l
-        self.w = w
-        self.color = color
-    def draw(self,display):
-        pygame.draw.rect(display, self.color, (self.x, self.y, self.w, self.h))
+class box():
+    def __init__(self,x,y,l,w,color,display):
+        box.x = x
+        box.y = y
+        box.l = l
+        box.w = w
+        box.color = color
+
+    def draw(win,self):
+
+        pygame.draw.rect(display, self.color, (self.x, self.y, self.w, self.l))
+
 
 pygame.init()
-def refresh():
-    display.fill((black))
-    pygame.draw.rect(display, red, (1, 1, 10, 10))
-    pygame.display.update()
 
+
+
+t = []
+j=0
 run = True
-thing = box(10,10,10,10,red)
 display_width = 550
 display_height =550
 display= pygame.display.set_mode((display_width, display_height))
+display.fill((black))
+for i in range(1,10):
+    for l in range(1,10):
+        t.append(box((i*50),(l*50),50,50,red,display))
+        j+=1
+        box.draw(display,box)
+
+
+
+
 pygame.display.set_caption("Maze")
+
 while run:
     pygame.time.delay(100)
     for event in pygame.event.get():
         if event.type ==pygame.QUIT:
             run  = False
-    refresh()
+
+    box.draw(display,box)
+
+    pygame.display.update()
 
 
 pygame.quit()
