cb34a84ea1e8146570146402bbcef82b0cb2e1ef
Parth-Mac <parthkj6@gmail.com>
2022-04-11 21:46:11 -0400

    Made a seperate file to make the boxes

---
+++ Cleaned Up/makeboxes.py
@@ -0,0 +1,8 @@
+import box
+black = (0,0,0)
+def main(num, dementions, dis):
+    tempthing = []
+    for i in range(1,num):
+        for l in range(1,num):
+            tempthing.append(box.part(i*dementions,l*dementions,dementions,dementions,black,dis))
+    return tempthing
